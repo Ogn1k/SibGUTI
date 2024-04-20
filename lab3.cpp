8eaac4feef6afc20551db033ef928debdd32eeff
Ogn1k <62839671+Ogn1k@users.noreply.github.com>
2024-04-20 12:21:17 +0700

    lab3

--- lab3.cpp
+++ lab3.cpp
@@ -2,6 +2,7 @@
 #include <locale.h>
 #include <vector>
 #include <sstream>
+#include <algorithm>
 
 bool isFloat(const std::string& str) 
 {
@@ -19,10 +20,26 @@ float stringToFloat(const std::string& str)
     return result;
 }
 
+bool Next_p(std::vector<int>& arr, int n)
+{
+    int k = n-2;
+    while(k>=0 && arr[k] >= arr[k+1])
+    {
+        k-=1;
+        if(k==-1) return false;
+    }
+    int l = n-1;
+    while(arr[k] >= arr[l])
+        l-=1;
+    std::swap(arr[k], arr[l]);
+    std::reverse(arr.begin(), arr.end());
+    return true;
+}
+
 int main()
 {
     setlocale(LC_ALL, "");
-    std::vector<float> a;
+    std::vector<int> a;
     std::string aa = "";
     
     std::cout << "enter array\n";
@@ -32,8 +49,15 @@ int main()
         if(isFloat(aa))
         {
             a.insert(a.begin(), stringToFloat(aa));
-        }       
+        }
     }
+    std::sort(a.begin(), a.end());
 
+    do
+    {
+        for(int n : a) std::cout << n << " ";
+        std::cout << "\n";
+    }
+    while(Next_p(a,a.size()));
     return 0;
 }
